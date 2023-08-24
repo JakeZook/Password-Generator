b1cc2f1d72a5ce8f7967dc83e7d9d109b0a055e8
Jake Zook <jakezook01@gmail.com>
2023-08-24 16:05:15 -0600

    All possible characters will now form one large array and a password is generated off random elements inside the array

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordLength;
 var lowercaseIncluded;
 var uppercaseIncluded;
 var numeralsIncluded;
 var specialCharactersIncluded;
+
+const lowercaseCharacters = ["a", "b", "c", "d", "e",
+"f", "g", "h", "i", "j", 
+"k", "l", "m", "n", "o", 
+"p", "q", "r", "s", "t", 
+"u", "v", "w", "x", "y", "z"];
+
+const uppercaseCharacters = ['A','B','C','D','E',
+'F','G','H','I','J',
+'K','L','M','N','O',
+'P','Q','R','S','T',
+'U','V','W','X','Y','Z'];
+
+const numeralCharacters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+const specialCharacters = ["!", "@", "#", "$", "%",
+"^", "&", "*", "(", ")",
+"=", "+", "-", "~", "/",
+"<", ">", "?"];
+
+var possibleCharacters = [];
 
 // Write password to the #password input
 function writePassword() {
@@ -17,16 +39,25 @@ function writePassword() {
 function generatePassword() {
 	getLength();
 	getCharacterTypes();
+	assignCharacters();
+
+	var generatedPassword = "";
+	for (let i = 0; i < passwordLength; i++)
+	{
+		let char = Math.floor(Math.random() * possibleCharacters.length);
+		generatedPassword += possibleCharacters[char];
+	}
+	return generatedPassword;
 }
 
 function getLength() {
 	var validLength = false;
 	while (!validLength) {
-		var length = prompt(
+		passwordLength = prompt(
 			"Enter the length of the password! Must be between 8-128 characters!"
 		);
 
-		if (length >= 8 || length <= 128) {
+		if (passwordLength >= 8 || passwordLength <= 128) {
 			validLength = true;
 		}
 	}
@@ -39,6 +70,33 @@ function getCharacterTypes() {
 	specialCharactersIncluded = confirm(
 		"Would you like to include special characters?"
 	);
+}
+
+function assignCharacters() {
+	possibleCharacters = [];
+
+	if (
+		!lowercaseIncluded &&
+		!uppercaseIncluded &&
+		!numeralsIncluded &&
+		!specialCharactersIncluded
+	) {
+		alert("You must include at least one character type!");
+		getCharacterTypes();
+	}
+
+	if (lowercaseIncluded) {
+		possibleCharacters.push(...lowercaseCharacters);
+	}
+	if (uppercaseIncluded) {
+		possibleCharacters.push(...uppercaseCharacters);
+	}
+	if (numeralsIncluded) {
+		possibleCharacters.push(...numeralCharacters);
+	}
+	if (specialCharactersIncluded) {
+		possibleCharacters.push(...specialCharacters);
+	}
 }
 
 // Add event listener to generate button
